7e56bb0817b046854928e41751ac753382f9099b
misabiko <misabiko@gmail.com>
2024-09-26 17:16:23 -0400

    hardcoded gerstner waves in ShaderLab

--- Assets/Scripts/OceanMeshGenerator.cs
+++ Assets/Scripts/OceanMeshGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class OceanMeshGenerator : MonoBehaviour {
 	Mesh mesh;
 	
