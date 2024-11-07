59f11da9fe47c2f37b30b4736cfe7c17f0b455de
Souchy <robyn.g@hotmail.fr>
2024-11-06 23:56:20 -0500

    Wip Bounds steering

--- Assets/Scripts/BirdSimulation.cs
+++ Assets/Scripts/BirdSimulation.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.InputSystem.HID.HID;
 
 public class BirdSimulation : MonoBehaviour
 {
@@ -6,7 +7,6 @@ public class BirdSimulation : MonoBehaviour
     public GameObject boidPrefab;
     public int boidCount = 20;
     public GameObject[] allBoids;
-    public Vector3 spaceBounds = new Vector3(5, 5, 5);
     public Vector3 goalPos =  new Vector3(0, 0, 0);
 
     [Header("Boid settings")]
@@ -31,7 +31,13 @@ public class BirdSimulation : MonoBehaviour
     [Range(0f, 100.0f)]
     public float WanderWeight;
     [Range(0f, 100.0f)]
+    public float BoundAvoidanceWeight;
+    [Range(0f, 100.0f)]
     public float VelocityLerp;
+    [Range(0f, 100.0f)]
+    public float MaxSpeed = 5f;
+    [Range(10f, 100.0f)]
+    public float SpaceBoundRadius;
 
 
 
@@ -41,11 +47,7 @@ public class BirdSimulation : MonoBehaviour
         allBoids = new GameObject[boidCount];
         for (int i = 0; i < boidCount; i++)
         {
-            Vector3 pos = this.transform.position + new Vector3(
-                Random.Range(-spaceBounds.x, spaceBounds.x),
-                Random.Range(-spaceBounds.y, spaceBounds.y),
-                Random.Range(-spaceBounds.z, spaceBounds.z)
-            );
+            Vector3 pos = this.transform.position + Random.insideUnitSphere * SpaceBoundRadius;
             allBoids[i] = Instantiate(boidPrefab, pos, Quaternion.identity);
         }
         instance = this;
@@ -55,13 +57,18 @@ public class BirdSimulation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Random.Range(0, 100) < 10)
+        if (Random.Range(0, 100) < 1)
         {
-            goalPos = this.transform.position + new Vector3(
-                Random.Range(-spaceBounds.x, spaceBounds.x),
-                Random.Range(-spaceBounds.y, spaceBounds.y),
-                Random.Range(-spaceBounds.z, spaceBounds.z)
-            );
+            goalPos = Vector3.Lerp(goalPos, this.transform.position + Random.insideUnitSphere * SpaceBoundRadius, 0.5f);
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0);
+        Gizmos.DrawWireSphere(this.goalPos, 1);
+        Gizmos.color = new Color(1, 0, 0);
+        Gizmos.DrawWireSphere(Vector3.zero, SpaceBoundRadius);
+    }
+
 }
--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -4,15 +4,15 @@ using Random = UnityEngine.Random;
 
 public class Boid : MonoBehaviour
 {
-    //float speed;
-    const float Speed = 5f;
     public Vector3 velocity;
     public Vector3 centroid;
+
+    private BirdSimulation Simulation => BirdSimulation.instance;
 
     // Start is called before the first frame update
     void Start()
     {
-        velocity = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized * Random.Range(1, Speed);
+        velocity = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized * Random.Range(1, Simulation.MaxSpeed);
     }
 
     // Update is called once per frame
@@ -26,71 +26,84 @@ public class Boid : MonoBehaviour
     {
         Vector3 avgPos = Vector3.zero;
         Vector3 avgVel = Vector3.zero;
-        Vector3 close_d = Vector3.zero;
+        Vector3 avoidance = Vector3.zero;
 
         var previousVelocity = this.velocity;
-
         var pos0 = this.transform.position;
         var vel0 = this.velocity;
-
-        Vector3 wander = vel0 * (float) Math.Tan(2f * Math.PI / 12f);
-        wander *= (float) Math.Sin(DateTime.Now.Ticks / TimeSpan.TicksPerSecond);
 
         // Flocking
         int countInProximity = 0;
         int countInAvoidance = 0;
-        foreach (var boid in BirdSimulation.instance.allBoids)
+        foreach (var boid in Simulation.allBoids)
         {
             var b = boid.GetComponent<Boid>();
             if (b == this)
                 continue;
 
             var pos1 = boid.transform.position;
-            var vel1 = b.velocity;
-            var dist = pos1 - pos0;
-            var d = dist.magnitude;
-            if (d <= BirdSimulation.instance.AvoidanceRadius)
+            var deltaPos = pos1 - pos0;
+            var dist = deltaPos.magnitude;
+            if (dist <= Simulation.AvoidanceRadius)
             {
                 countInAvoidance++;
-                close_d += -dist.normalized * (BirdSimulation.instance.AvoidanceRadius - dist.magnitude);
+                avoidance += -deltaPos.normalized * (Simulation.AvoidanceRadius - deltaPos.magnitude);
             }
             else
-            if (d <= BirdSimulation.instance.DetectRadius) // && Vector3.Dot(vel0.normalized, dist) > -0.5f)
+            if (dist <= Simulation.DetectRadius) // && Vector3.Dot(vel0.normalized, dist) > -0.5f)
             {
                 countInProximity++;
                 avgPos += pos1;
-                avgVel += vel1;
+                avgVel += b.velocity;
             }
         }
 
-        // Apply
+        // Wander
+        Vector3 wander = vel0 * (float) Math.Tan(2f * Math.PI / 12f);
+        wander *= (float) Math.Sin(DateTime.Now.Ticks / TimeSpan.TicksPerSecond);
+        vel0 += wander * Simulation.WanderWeight;
+
+
+        Vector3 cohesion = Vector3.zero;
+        Vector3 align = Vector3.zero;
+        // Cohesion + Alignment
         if (countInProximity > 0)
         {
             avgPos /= countInProximity;
             avgVel /= countInProximity;
             centroid = avgPos;
 
-            var cohesion = (avgPos - pos0) * BirdSimulation.instance.Cohesion;
-            var align = (avgVel - vel0) * BirdSimulation.instance.Alignment;
+            cohesion = (avgPos - pos0) * Simulation.Cohesion;
+            align = (avgVel - vel0) * Simulation.Alignment;
             Debug.DrawLine(this.transform.position, this.transform.position + cohesion, Color.green);
             Debug.DrawLine(this.transform.position, this.transform.position + align, Color.blue);
             vel0 += cohesion + align;
         }
+        // Avoidance
         if (countInAvoidance > 0)
         {
             //close_d /= countInAvoidance;
-            vel0 += close_d * BirdSimulation.instance.Separation;
+            vel0 += avoidance * Simulation.Separation;
         }
 
-        vel0 += wander * BirdSimulation.instance.WanderWeight;
-        vel0 += (BirdSimulation.instance.goalPos - this.transform.position).normalized * BirdSimulation.instance.GoalWeight;
+        // Goal
+        vel0 += (Simulation.goalPos - this.transform.position).normalized * Simulation.GoalWeight;
 
-        // Clamp
-        var speed = Math.Clamp(vel0.magnitude, 1, Speed);
+        // Avoid Bounds
+        var forwardDetection = this.transform.position + vel0.normalized * Simulation.DetectRadius;
+        if(forwardDetection.magnitude > Simulation.SpaceBoundRadius)
+        {
+            var c = Vector3.Cross(forwardDetection, this.transform.position);
+            //vel0 += c * Simulation.BoundAvoidanceWeight;
+            vel0 += -this.transform.position.normalized * Simulation.BoundAvoidanceWeight;
+        }
+
+        // Clamp speed
+        var speed = Math.Clamp(vel0.magnitude, 1, Simulation.MaxSpeed);
         vel0 = vel0.normalized * speed;
-        //vel0 = (vel0 + previousVelocity) / 2.0f;
-        vel0 = Vector3.Lerp(previousVelocity, vel0, BirdSimulation.instance.VelocityLerp);
+        vel0 = Vector3.Lerp(previousVelocity, vel0, Simulation.VelocityLerp);
         this.velocity = vel0;
+
 
         // Transform
         Debug.DrawLine(this.transform.position, this.transform.position + velocity, Color.red);
@@ -98,17 +111,18 @@ public class Boid : MonoBehaviour
         this.transform.Translate(velocity * Time.deltaTime, Space.World);
     }
 
+
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 1);
-        Gizmos.DrawWireSphere(this.centroid, BirdSimulation.instance.DetectRadius);
+        Gizmos.DrawWireSphere(this.centroid, Simulation.DetectRadius);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(0, 1, 1);
-        Gizmos.DrawWireSphere(this.transform.position, BirdSimulation.instance.DetectRadius);
-        Gizmos.DrawWireSphere(this.transform.position, BirdSimulation.instance.AvoidanceRadius);
+        Gizmos.DrawWireSphere(this.transform.position, Simulation.DetectRadius);
+        Gizmos.DrawWireSphere(this.transform.position, Simulation.AvoidanceRadius);
     }
 
 }
