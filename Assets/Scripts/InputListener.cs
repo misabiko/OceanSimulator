c772a3165e0209128381617e2b88ca2608661f3f
Malanti <malanto.rakotondrabe.1@ens.etsmtl.ca>
2024-11-26 05:25:01 +0100

    Animations bateau

---
+++ Assets/Scripts/InputListener.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputListener : MonoBehaviour
+{
+    PlayerInput playerInput;
+    private BoatInputAction _inputActions;
+    private Boolean isMoving = false;
+
+    [SerializeField] private Vector2 moveInput;
+    [SerializeField] private Component rightpaddle;
+    [SerializeField] private Component leftpaddle;
+    private void Awake()
+    {
+        _inputActions = new BoatInputAction();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _inputActions.BoatController.Moveforward.performed += onMove;
+        _inputActions.BoatController.Moveforward.canceled += stopMove;
+        _inputActions.Enable();
+    }
+    private void onMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        // Dampen towards the target rotation
+        isMoving = true;
+    }
+
+    private void stopMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        // Dampen towards the target rotation
+        isMoving = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime;
+        if (isMoving)
+        {
+            rightpaddle.transform.Rotate(0, 0, 2.5f);
+            leftpaddle.transform.Rotate(0, 0, 2.5f);
+        }
+    }
+}
