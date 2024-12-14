f7780eb479b67ce36e9af0f385fb381ed4968c1a
Malanti <malanto.rakotondrabe.1@ens.etsmtl.ca>
2024-12-14 23:20:04 +0100

    Re-Added wingflap sound effect

--- Assets/Scripts/3Cs/BirdControllerAnimationEvents.cs
+++ Assets/Scripts/3Cs/BirdControllerAnimationEvents.cs
@@ -5,5 +5,8 @@ public class BirdControllerAnimationEvents : MonoBehaviour
     public void PlayWingFlapSFX()
     {
         SoundManager.HandleLocalPlaySound("Wing Flap");
+        if (PlayerStateManager.GetState() == PlayerState.Bird){
+            AudioManager.instance.PlayOneShot(FMODEvents.instance.wingFlap, this.transform.position);
+        }
     }
 }
--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -6,6 +6,9 @@ public class FMODEvents : MonoBehaviour
     [field: Header("WaveCrash")]
     [field: SerializeField] public EventReference waveLtoR { get; private set; }
     [field: SerializeField] public EventReference waveRtoL { get; private set; }
+
+    [field: Header("Bird")]
+    [field: SerializeField] public EventReference wingFlap { get; private set; }
     public static FMODEvents instance { get; private set; }
 
     private void Awake()
