e31abfce8f3ee8a84bd89ada374011f2dc4790ce
misabiko <misabiko@gmail.com>
2024-12-16 11:16:46 -0500

    Added error for audio manager

--- Assets/Scripts/3Cs/BoatController.cs
+++ Assets/Scripts/3Cs/BoatController.cs
@@ -65,6 +65,8 @@ public class BoatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+	    if (AudioManager.instance == null)
+		    Debug.LogError("AudioManager is missing in the scene");
         if (Input.GetKeyDown(KeyCode.M))
             PlayerStateManager.SwitchTo(PlayerState.Bird);
 
